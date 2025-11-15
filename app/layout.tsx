06e50e46574e089ef15f5ef01da5c81a218ca88f
Keshav1605 <keshavjais1605@gmail.com>
2025-11-15 13:58:22 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,19 +16,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "https://img.icons8.com/?size=100&id=okOWzJV55XPz&format=png&color=000000",
+        type: "image/png",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "https://img.icons8.com/?size=180&id=okOWzJV55XPz&format=png&color=000000",
   },
   viewport: {
     width: "device-width",
